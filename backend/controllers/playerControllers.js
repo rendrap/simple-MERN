c4bda917b3bee927d950bd1a25117ea969184a9f
rendrap <rendrapradana@gmail.com>
2021-03-01 21:42:31 +0700

    add CRUD

--- backend/controllers/playerControllers.js
+++ backend/controllers/playerControllers.js
@@ -23,3 +23,32 @@ export const getPlayers = (req, res) => {
     res.json(Player);
   });
 };
+
+export const getPlayerWithID = (req, res) => {
+
+  Player.findById(req.params.PlayerId,(err, Player) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(Player);
+  });
+};
+
+export const updatePlayer = (req, res) => {
+  Player.findOneAndUpdate({_id: req.params.PlayerId}, req.body, {new: true}, (err, Player) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(Player);
+  });
+};
+
+export const deletePlayer = (req, res) => {
+
+  Player.remove({ _id: req.params.PlayerId },(err, Player) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json({message: 'Sucessfully deleted player'});
+  });
+};
--- backend/routes/soccerRoutes.js
+++ backend/routes/soccerRoutes.js
@@ -1,4 +1,10 @@
-import { addNewPlayer,getPlayers } from '../controllers/playerControllers.js';
+import {
+ addNewPlayer,
+getPlayers,
+getPlayerWithID,
+updatePlayer,
+deletePlayer
+} from '../controllers/playerControllers.js';
 
 const routes = (app) => {
   app.route('/players')
@@ -7,6 +13,15 @@ const routes = (app) => {
 
     // POST endpoint
     .post(addNewPlayer);
+
+  app.route('/player/:PlayerId')
+  // Get specific player
+  .get(getPlayerWithID)
+  // Update specific player
+  .put(updatePlayer)
+  // Delete specific player
+  .delete(deletePlayer)
+
 }
 
 export default routes;
